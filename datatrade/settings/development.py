80a77893213ce144390a54a78ab38ce9ff45a340
rb <rbiojout@gmail.com>
2018-11-19 18:23:23 +0100

    adjust config

--- datatrade/settings/development.py
+++ datatrade/settings/development.py
@@ -1,10 +1,11 @@
 from .base import *
 
+print("IN DEVELOPMENT CONFIG")
 
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = congig('DJANGO_SECRET_KEY', 'Wg2590HFq9nP7k7m8GZYtv1pleG0R5d2XOuS990BorWWxtBSidusjCNwdBm4pRfx')
+SECRET_KEY = config('DJANGO_SECRET_KEY', 'Wg2590HFq9nP7k7m8GZYtv1pleG0R5d2XOuS990BorWWxtBSidusjCNwdBm4pRfx')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default=['*.amaezonaws.com', 'ductilis.com'])
 
--- datatrade/settings/production.py
+++ datatrade/settings/production.py
@@ -4,7 +4,7 @@ from .base import *
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = congig('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default=['ductilis.com'])
 
