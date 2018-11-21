b10265a1c3573aaa8f8ca30953291fe2c24f42ad
rb <rbiojout@gmail.com>
2018-11-21 21:08:04 +0100

    corrected hosts for development

--- datatrade/settings/development.py
+++ datatrade/settings/development.py
@@ -7,7 +7,7 @@ print("IN DEVELOPMENT CONFIG")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = config('DJANGO_SECRET_KEY', 'Wg2590HFq9nP7k7m8GZYtv1pleG0R5d2XOuS990BorWWxtBSidusjCNwdBm4pRfx')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default=['*.amaezonaws.com', 'ductilis.com'])
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default=['*.amazonaws.com', '*.ductilis.com'])
 
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
